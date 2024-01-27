225057d7b4f168db75c7e179f17a45ae0d868742
sanket2126 <sanketparmar2126@gmail.com>
2024-01-27 08:31:09 +0530

    Sherlock and Valid String solution added.

--- HackerRank_week3.playground/Pages/New_Year_Chaos.xcplaygroundpage/Contents.swift
+++ HackerRank_week3.playground/Pages/New_Year_Chaos.xcplaygroundpage/Contents.swift
@@ -1,9 +1,16 @@
 import UIKit
-/*
- It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from  to . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
 
- Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
- 
+// MARK: It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from  to . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+// MARK: Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+ /*
+  q = [1,2,3,5,4,6,7,8]
+  If person 5 bribes person 4, the queue will look like this: 1,2,3,5,4,6,7,8. Only 1 bribe is required. Print 1.
+
+  q = [4,1,2,3]
+  Person 4 had to bribe 3 people to get to the current position.
+  Print Too chaotic.
+  
  */
 
 func minimumBribes(q: [Int]) -> Void {
@@ -30,9 +37,12 @@ minimumBribes(q: [2,5,1,3,4])
 
 /*
  Input
- 
- some details and input output pending
- 
+ 2 1 5 3 4   q = [2, 1, 5, 3, 4]
  Output
+ 3
  
+ Input
+ 2 5 1 3 4   q = [2, 5, 1, 3, 4]
+ Output
+ Too Chaotic
  */
--- HackerRank_week3.playground/Pages/Sherlock_Valid_String.xcplaygroundpage/Contents.swift
+++ HackerRank_week3.playground/Pages/Sherlock_Valid_String.xcplaygroundpage/Contents.swift
@@ -1,7 +1,48 @@
-//: [Previous](@previous)
+// MARK: Sherlock considers a string to be valid if all characters of the string appear the same number of times. It is also valid if he can remove just 1 character at 1 index in the string, and the remaining characters will occur the same number of times. Given a string S, determine if it is valid. If so, return YES, otherwise return NO.
+ 
+/*
+ String S = "abc"
+ This is a valid string because frequencies are .{a:1, b:1, c:1}
+ 
+ String S = "abcc"
+ This is a valid string because we can remove one c and have 1 of each character in the remaining string.
+ */
 
-import Foundation
+func isValid(s: String) -> String {
+  var dictCount = [String: Int]()
+  var valCount = [Int: Int]()
+  for e in s {
+    dictCount["\(e)"] = (dictCount["\(e)"] ?? 0) + 1
+  }
+  for e in dictCount.values {
+    valCount[e] = (valCount[e] ?? 0) + 1
+  }
+  if valCount.count == 1 {
+    return "YES"
+  } else if (valCount.count == 2) {
+    let k1 = valCount.keys.min() ?? 0
+    let k2 = valCount.keys.max() ?? 0
+    if (valCount[k1] == 1 && k1 == 1) || (valCount[k2] == 1 && k2 == 1) {
+      return "YES"
+    }
+    if (abs(k1 - k2) == 1) && (valCount[k1] == 1 || valCount[k2] == 1) {
+     return "YES"
+    }
+  }
+  return "NO"
+}
 
-var greeting = "Hello, playground"
+print(isValid(s: "aabbcd"))
 
-//: [Next](@next)
+print(isValid(s: "abcdefghhgfedecba"))
+
+/*
+ Example
+ String S = aabbcd
+ 
+ 2 is the minimum number of removals required to make it a valid string. It can be done in following two ways:
+
+ Remove c and d to get aabb.
+ Or remove a and b to get abcd.
+ 
+ */
