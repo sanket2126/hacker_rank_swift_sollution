b6281a33962553d579c9922d0fcef4be5974aeb2
sanket2126 <sanketparmar2126@gmail.com>
2024-02-13 09:28:46 +0530

    Discord channel Pair_upto_100 solution added in swift.

--- Discord_IOS_DSA.playground/Pages/PairUpto_100.xcplaygroundpage/Contents.swift
+++ Discord_IOS_DSA.playground/Pages/PairUpto_100.xcplaygroundpage/Contents.swift
@@ -18,26 +18,4 @@ func combination(_ nums: [Int]) -> String {
     return combinations.map({"{\($0.key), \($0.value)}"}).joined(separator: " ")
 }
 
-//print(combination([10,70,40,30,20,60,80,90]))
-
-func commonChars(_ words: [String]) -> [String] {
-    var dict = words[0].reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
-    var result = [String]()
-    
-    for index in 1 ..< words.count {
-        let tempDict = words[index].reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
-        
-        for item in dict {
-            if let value = tempDict[item.key] {
-                dict[item.key] = min(value, item.value)
-            } else {
-                dict[item.key] = nil
-            }
-        }
-    }
-    
-    dict.forEach { result += Array(repeating: String($0.key), count: $0.value) }
-    return result
-}
-
-print(commonChars( ["good","hud","mood","fill","till"]))
+print(combination([10,70,40,30,20,60,80,90]))s
