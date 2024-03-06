81237c2cdc1f9ad2a453774d42fbc7bc189f9fab
sanket2126 <sanketparmar2126@gmail.com>
2024-03-06 13:48:22 +0530

    Solution for Count Element with Maximum Frequency [easy].

---
+++ LeetCode_easy.playground/Pages/3005_CountEleMaxFreq.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+
+//MARK: 3005. Count Elements With Maximum Frequency
+
+//https://leetcode.com/problems/count-elements-with-maximum-frequency/description/?envType=daily-question&envId=2024-03-08
+
+func maxFrequencyElements(_ nums: [Int]) -> Int {
+    var max = 0
+    var sum = 0
+    var freqDict = [Int:Int]()
+    for n in nums {
+        freqDict[n, default: 0] += 1
+    }
+    for (_,f) in freqDict {
+        if f > max {
+            max = f
+            sum = f
+        } else if f == max {
+            sum += f
+        }
+    }
+    return sum
+}
+
+print(maxFrequencyElements([1,2,2,3,1,4]))
+
+print(maxFrequencyElements([1,2,3,4,5]))
