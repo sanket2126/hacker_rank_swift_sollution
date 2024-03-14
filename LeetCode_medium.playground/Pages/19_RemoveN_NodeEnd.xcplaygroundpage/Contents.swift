66826c8a73c5d0aaf64a32ef202c72b4b90dbd61
sanket2126 <sanketparmar2126@gmail.com>
2024-03-14 19:44:25 +0530

    Solution for Remove Nth node from the end of LinkedList[medium].

---
+++ LeetCode_medium.playground/Pages/19_RemoveN_NodeEnd.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+// MARK: 19. Remove Nth Node From End of List
+
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+    var head1 = head
+    if (head == nil) || (head?.next == nil) {
+        return nil
+    } else {
+        var data = [Int]()
+        while head1?.next != nil {
+            data.append(head1?.val ?? 0)
+            head1 = head1?.next
+        }
+        data.append(head1?.val ?? 0)
+        var result: ListNode?
+        var prev : ListNode?
+        head1 = nil
+        for i in 0..<data.count {
+            if i == data.count-n {
+                continue
+            } else {
+                result = ListNode(data[i])
+                if (head1 == nil) { head1 = result }
+                if prev != nil { prev?.next = result }
+                prev = result
+            }
+        }
+        return head1
+    }
+}
+
+func printList(_ had: ListNode?) {
+    var arr = [Int]()
+    var head = had
+    while head != nil {
+        arr.append(head?.val ?? 0)
+        head = head?.next
+    }
+    print("Result -", arr)
+}
+
+let List1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+printList(removeNthFromEnd(List1, 2))
+
+
+let List2 = ListNode(1)
+printList(removeNthFromEnd(List2, 1))
+
+let List3 = ListNode(1, ListNode(2))
+printList(removeNthFromEnd(List3, 1))
+
+
